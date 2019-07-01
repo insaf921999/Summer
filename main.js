ec7dacdad5c26fe9d5c6c33e02993198c29b209f
insaf921999 <47789008+insaf921999@users.noreply.github.com>
2019-07-01 16:39:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// function changeColor(){
+// document.getElementById("myPara").style.color="red";}
+
+function underline(){
+document.getElementById("myPara").style.textDecoration="Underline";
+}
+
+function bold(){
+    document.getElementById("myPara").style.fontWeight="Bold";
+}
+
+function italic(){
+    document.getElementById("myPara").style.fontStyle="italic";
+}
+
+
+function change_size(){
+    document.getElementById("myPara").style.fontSize=size.value;
+}
+
+function change_font(){
+    document.getElementById("myPara").style.fontFamily=font.value;
+}
+
+function change_color(){
+    document.getElementById("myPara").style.color=color.value;
+
+}
